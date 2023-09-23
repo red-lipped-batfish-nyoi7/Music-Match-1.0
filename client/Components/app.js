9c56659381a9f31376e58de2579056651a1a17e6
jordanlim1 <jordanlim2015@gmail.com>
2023-09-23 16:09:53 -0400

    in progress

--- client/Components/app.js
+++ client/Components/app.js
@@ -1,14 +1,28 @@
 import React, { useState, useEffect } from "react";
 import SignUp from "./signup";
 import '../styles.scss'
+// import { Router } from "express";
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import MainPage from "./mainpage"
 
 const App = () => {
 
     return (
-        <div>
-            <SignUp />
 
+        <div>
+            <SignUp/>
         </div>
+        
+    //     <Router>
+    //         <Switch>
+    //         {/* <Route path="/main" component={MainPage} /> */}
+    //         <Route path ='/signup'> <SignUp/> </Route>
+    //         </Switch>
+            
+    // </Router>
+        
+       
+        
     )
 }
 

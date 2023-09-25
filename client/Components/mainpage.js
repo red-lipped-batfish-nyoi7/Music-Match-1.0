de829f1880300dfeff7036249c51285db7240fd6
jordanlim1 <jordanlim2015@gmail.com>
2023-09-25 17:24:32 -0400

    scratch project done

--- client/Components/mainpage.js
+++ client/Components/mainpage.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 
+
 //Main page should display:
     //your name
     //your age
@@ -7,34 +8,63 @@ import React, {useState, useEffect} from 'react';
     //your artists
 
 
-
+    
 const MainPage = () => {
-    // try{
-    //     const response = await fetch('/userprofile');
-    //     const userAndMatches = await response.json();
-    //     // .then(response => response.json())
+
+    
+    const [user, setUser] = useState([]);
+    const [match, setMatch] = useState([]);
+
+    // async function useFetch(){
+
+    //     let response = await fetch('/userprofile');
+    //     let data  = await response.json();
+        
+    //  setUser(data.userProfile)
+    
+    //     console.log('response' , data.userProfile)
+    //     console.log('response2' , data.matchesProfiles[0])
+    // return;
+    // }
+
+
+
+        useEffect(async () => {
+            let response = await fetch('/userprofile');
+            let data  = await response.json();
+            setUser(data.userProfile)
+            setMatch(data.matchesProfiles[0])
+        }, [])
     // }catch(err){
     //     console.error("An error occurred:", err);
     //     setMessage("An error occurred while fetching profile info.");
     // }
 
-
+  
 
     return (
-        <div class='card-container'>
-            <span class='profile'>
-            <Profile/>
-            {/* <Profile info={userAndMatches.userProfile}/> */}
-            </span>
-            {/* <span class='matches'>
-            <Matches/>
-            <Matches info={userAndMatches.matchesProfiles}/>
-            </span> */}
-        </div>
+
+        <div className="card-container">
+                 
+
+    <div className="card left-card">
+      <span className="profile">
+        <Profile inf={user} />
+      </span>
+    </div>
+
+    <img id = 'gif' src = 'https://media.tenor.com/Ez2x8MFvP0QAAAAC/heart-heartbeat.gif'/>
+
+    <div className="card right-card">
+      <span className="matches">
+        <MatchProfile inf={match} />
+      </span>
+    </div>
+  </div>
     )
 }
 
-// const Matches = () => { //took out PROPS
+// const Matches = (props) => { //took out PROPS
 //     //props is an array of objects of matches for the user
 //         //this array could have 0 things or many things!
 //     const matchContent = [];
@@ -63,18 +93,67 @@ const MainPage = () => {
 //     )
 // }
 
-const Profile =  () => {
+const Profile =  (props) => {
 
     //I need to query/fetch the profile name and photo from the DB
     // const { username, name, age, bio, artists } = props.info;
     const profilePhoto = ''; //what is this coming back as? TBD
-    
+    let {name, age, bio, artists, images} = props.inf
+    console.log(images)
+    // let people = Object.keys(props.inf.artists).join(', ')    
+    // console.log('arr', people)
+    let people = ''
+    for(let artist in artists){
+        people = people + artist + " "
+    }
+    console.log('art', people)
     return (
     <div>
+        
+
         {/* <h1>{name}</h1> */}
         <img class='round' src='https://ih1.redbubble.net/image.1530009255.8904/st,small,507x507-pad,600x600,f8f8f8.jpg'></img>
+        <div id='profile'>
+        <p>Name: {name}</p>
+        <p>Age: {age}</p>
+        <p>Favorite Artists: 
+            {people}</p>
+        <p>More About Myself: {bio}</p>
+        </div>
         {/* <Bio text={bio}/> */}
-        <Bio />
+        {/* <Bio /> */}
+    </div>
+    )
+}
+
+const MatchProfile =  (props) => {
+
+    //I need to query/fetch the profile name and photo from the DB
+    // const { username, name, age, bio, artists } = props.info;
+    const profilePhoto = ''; //what is this coming back as? TBD
+    let {name, age, bio, artists} = props.inf
+
+
+    let people = ''
+    for(let artist in artists){
+        people = people + artist + "\n"
+    }
+
+    return (
+    <div>
+        
+
+        {/* <h1>{name}</h1> */}
+        <img class='round' src='https://ih1.redbubble.net/image.1530009255.8904/st,small,507x507-pad,600x600,f8f8f8.jpg'></img>
+        <div id='profile'>
+        <p>Name: {name}</p>
+        <p>Age: {age}</p>
+        <p>Favorite Artists: {people}</p>
+
+        <p>More About Myself: {bio}</p>
+        </div>
+        {/* <Bio text={bio}/> */}
+        {/* <Bio /> */}
     </div>
     )
 }
@@ -82,15 +161,15 @@ const Profile =  () => {
 //endpoints: /userprofile for user info, /matchesprofiles for matches (these are two separate requests)
 const userBio = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
 
-const Bio = () => {
-    // const userBio = 'Hello Hello Hello Hello'
-        //i need to fetch this bio from the DB too
-    return (
-        <div id = 'text'>
-            {/* <p>{props.text}</p> */}
-            <p>{userBio}</p>
-        </div>
-    )
-}
+// const Bio = (props) => {
+//     // const userBio = 'Hello Hello Hello Hello'
+//         //i need to fetch this bio from the DB too
+//     return (
+//         <div id = 'text'>
+//             {/* <p>{props.text}</p> */}
+//             <p>{userBio}</p>
+//         </div>
+//     )
+// }
 
 export default MainPage;

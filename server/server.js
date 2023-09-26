c66764329ad9ae8d6c1bc50f5465c40d62383484
jeffnzk <jchan7240@gmail.com>
2023-09-26 18:54:32 -0400

    duwni

--- server/server.js
+++ server/server.js
@@ -17,29 +17,9 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-
 app.use('/build', express.static(path.join(__dirname, '../build')));
 
-app.get('/', (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, '../client/index.html'))
-});
-//this is basic homepage with login and signup 
-app.get('/signup', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, '../client/index.html'))
-});
 
-app.get('/main', (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, '../client/index.html'))
-});
-
-// app.get('/userprofile', (req, res) => { //hardcoded version for frontend testing, blocks the subsequent router
-//   res.status(200).json({
-//       userProfile: {
-//         username: 'iLikeTarik', name: 'Tarik', age: '99', bio: 'at codesmith rn', artists: ['Drakeo', 'Greedo']
-//       },
-//       matchesProfiles: [{username: 'iLikeTarik2', name: 'Tarik2', age: '98', bio: 'at codesmith rn', artists: ['Drakeo', 'Greedo']}]
-//   });
-// });
 app.get('/userprofile', controller.findProfileAndMatches, (req, res) => {
   res.status(200).json(res.locals.pageinfo);
 });
